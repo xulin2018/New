6974d8aac51d6328201a30c911d46aed86beaa27
xulin2018 <1927404906@qq.com>
2018-03-09 22:09:00 +0800

    完整版汽车网站

--- qiche2/js/main.js
+++ qiche2/js/main.js
@@ -7,22 +7,23 @@ $(".focusbox").slide ({
     autoPlay: true,
 //      自动运行
     deleteTime: 1000,
-//    特效切换的持续时间1秒，一次切换效果执行所用的时间长度
-    interTime: 3000,
-//    特效切换的间隔时间3秒，自动运行间隔 无缝滚动时相当于运行速度
+//    特效切换的持续时间，一次切换效果执行所用的时间长度
+    interTime: 2000,
+//    特效切换的间隔时间2秒，自动运行间隔 无缝滚动时相当于运行速度
     effect: "leftLoop"
     //    动画效果向左循环, 最后一个不需要逗号
 })
 
 
 //租车流程 新闻动态
-$(".process-new").slide ({
-    mainCell: ".new-list li",            
-    interTime: 50,
-    effect: "topMarquee",
+$(".process-new .new-list").slide ({
+    mainCell: "ul", 
     autoPlay: true,
-    vis: 3
+    effect: "topMarquee",
+    vis: 5,
+    interTime: 50
 });
+
 
 
 //租车展示
